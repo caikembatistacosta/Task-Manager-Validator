f72550b2af3f31bf667f965f5ff5b34f814414a1
Vinicius Cesar <viniciusmaquine14@gmail.com>
2022-09-21 16:33:33 -0300

    Initial Commit

---
+++ Entities/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Enums;
+
+namespace Entities
+{
+    public class Cliente : Entity
+    {
+               
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public Genero Genero { get; set; }
+        public bool EstaAtivo { get; set; }
+        public int EnderecoID { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
