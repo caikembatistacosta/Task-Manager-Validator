6b9aac0818ba33f06db5036f7e09c83612d9b73e
Vinicius Cesar <viniciusmaquine14@gmail.com>
2022-09-21 17:12:52 -0300

    Caike

---
+++ Task-Manager-Validator/Entities/Estado.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Entities
+{
+    public class Estado : Entity
+    {
+
+        public string UF { get; set; }
+        public string NomeEstado { get; set; }
+    }
+}
---
+++ Task-Manager-Validator/Entities/Funcionario.cs
@@ -0,0 +1,40 @@
+﻿using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Funcionario 
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public Genero Genero { get; set; }
+        public NivelDeAcesso NivelDeAcesso { get; set; }
+        public bool IsAtivo { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+        public int Idade
+        {
+            get
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - DataNascimento.Year;
+                if (DataNascimento.Date > today.AddYears(-age))
+                {
+                    age--;
+                }
+                return age;
+
+            }
+
+        }
+    }
+}
